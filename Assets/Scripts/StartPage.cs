e4e98acfb0a9c77741c7b7cef9a64c82cc75adf4
jarenegoh <s10257934@connect.np.edu.sg>
2024-06-15 22:31:43 +0800

    Added comments into StartPage script

--- Assets/Scripts/StartPage.cs
+++ Assets/Scripts/StartPage.cs
@@ -3,6 +3,7 @@
  * Date: 13 June 2024
  * Description: Script that controls the Start Page
  */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,11 +13,13 @@ public class StartPage : MonoBehaviour
 {
     public void Play()
     {
+        // Loads the Game Scene when the 'Play' button is clicked
         SceneManager.LoadScene("Game");
     }
 
     public void Quit()
     {
+        // Quits the application 
         Application.Quit();
     }
 }
